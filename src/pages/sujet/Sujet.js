a6f19763f376f9db07bf150bb1475c1b1bde2214
Omar-ciber <omarsow388@gmail.com>
2023-12-20 16:12:12 +0000

    Initialisation

---
+++ src/pages/sujet/Sujet.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Sujet extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Sujet;
