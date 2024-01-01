26722f7dec3624e11f456b6c98f9047874d89e7f
Fatou Diop SOLY <solyfatoudiop@gmail.com>
2024-01-01 16:04:46 +0100

    sowlymodiff

--- src/sections/Apropos.js
+++ src/sections/Apropos.js
@@ -14,7 +14,7 @@ const Apropos = () => {
   
     const div2Style = {
       position: 'absolute',
-      top: '300px',
+      top: '400px',
       left: '50px',
       width: '350px',
       height: '80px',
@@ -28,16 +28,23 @@ const Apropos = () => {
         justifyContent: 'center',
         flexWrap: 'wrap',
         padding: '20px',
+        border: ''
         // Autres styles CSS pour votre conteneur
     };
     const imageStyle = {
         width: '400px',
-        height: '400px',
+        height: '500px',
         boxShadow: '2px 2px 4px rgba(0, 0, 0, 0.3)',
         // Autres styles pour votre image
     };
     const titleStyle = {
         color:'white',
+    };
+    const textStyle = {
+      color: 'black',
+      width: '700px',
+      padding: '20px',
+      fontSize: '20px',
     };
    
   
@@ -51,11 +58,24 @@ const Apropos = () => {
           style={imageStyle}
         />
         </div>
+
+        
+      {/* Nouvelle div pour le texte - à gauche de l'image */}
+      <div style={{ ...textStyle, position: 'absolute', left: '470px', top: '50px' }}>
+        <h2 style={{color: 'blue', }}>Nom de la plateforme</h2>
+        <p>
+          Nous sommes dédiés à faciliter la recherche académique en offrant une plateforme innovante proposant une variété de sujets de mémoire. Notre mission est de simplifier le processus de sélection de thèmes pertinents et stimulants pour les étudiants et les chercheurs.
+        </p>
+        <p>
+          Avec notre application, explorez une multitude d'idées pour inspirer et guider votre parcours académique vers l'excellence.
+        </p>
+      </div>
         
         {/* Div 2 en position absolue */}
         <div style={div2Style}>
             <h2 style={titleStyle}>A propos de nous</h2>
         </div>
+
       </div>
       
     );
